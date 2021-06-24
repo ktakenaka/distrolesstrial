08f7b484773f9171069a00b453b58ece932043d5
Kazumasa Takenaka <bzzs.1120@gmail.com>
2021-06-25 00:07:53 +0700

    reduce allocations to 0 docker build -f Dockerfile-alpine -t distrolesstrial:alpine . && docker run --rm -i -t distrolesstrial:alpine Allocs: 0 time taken: 561464600
    docker build -f Dockerfile-distroless -t distrolesstrial:distroless . && docker run --rm -i -t distrolesstrial:distroless
    Allocs: 0
    time taken: 58299090

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"hash"
 	"math/rand"
 	"os"
 	"runtime/pprof"
 	"strconv"
+	"sync"
 	"testing"
 	"time"
 	"unsafe"
@@ -16,21 +18,46 @@ const (
 	cpuProfFile = "cpu.pprof"
 )
 
-func foo(n int) string {
-	x := strconv.Itoa(n)
-	buf := make([]byte, 0, 100000*len(x))
-	for i := 0; i < 100000; i++ {
-		buf = append(buf, x...)
-	}
-	sum := sha256.Sum256(buf)
+var bufPool = sync.Pool{
+	New: func() interface{} {
+		b := make([]byte, 256)
+		return &b
+	},
+}
 
-	b := make([]byte, 0, int(sum[0]))
+var hashPool = sync.Pool{
+	New: func() interface{} {
+		return sha256.New()
+	},
+}
+
+func foo(n int) string {
+	bufptr := bufPool.Get().(*[]byte)
+	defer bufPool.Put(bufptr)
+
+	buf := *bufptr
+	buf = buf[:0]
+
+	h := hashPool.Get().(hash.Hash)
+	defer hashPool.Put(h)
+	h.Reset()
+
+	x := strconv.AppendInt(buf, int64(n), 10)
+	for i := 0; i < 100000; i++ {
+		h.Write(x)
+	}
+	buf = buf[:0]
+	sum := h.Sum(buf)
+
+	b := make([]byte, 0, 256)
 	for i := 0; i < int(sum[0]); i++ {
 		x := sum[(i*7+1)%len(sum)] ^ sum[(i*5+3)%len(sum)]
 		c := "abcdefghijklmnopqrstuvwxyz"[x%26]
 		b = append(b, c)
 	}
-	return *(*string)(unsafe.Pointer(&b))
+	sum = sum[:0]
+	sum = append(sum, b...)
+	return *(*string)(unsafe.Pointer(&sum))
 }
 
 func main() {
