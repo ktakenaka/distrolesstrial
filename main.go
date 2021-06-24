332235ce95367690bc7b4146068e9a5c15de5622
Kazumasa Takenaka <bzzs.1120@gmail.com>
2021-06-24 23:37:48 +0700

    reduce allocations to 3
    docker build -f Dockerfile-distroless -t distrolesstrial:distroless . && docker run --rm -i -t distrolesstrial:distroless
    Allocs: 3
    time taken: 660076300
    
    docker build -f Dockerfile-alpine -t distrolesstrial:alpine . && docker run --rm -i -t distrolesstrial:alpine
    Allocs: 3
    time taken: 670499200

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"crypto/sha256"
 	"fmt"
 	"math/rand"
 	"os"
 	"runtime/pprof"
 	"strconv"
-	"strings"
 	"testing"
 	"time"
+	"unsafe"
 )
 
 const (
@@ -18,19 +17,20 @@ const (
 )
 
 func foo(n int) string {
-	var buf bytes.Buffer
+	x := strconv.Itoa(n)
+	buf := make([]byte, 0, 100000*len(x))
 	for i := 0; i < 100000; i++ {
-		buf.WriteString(strconv.Itoa(n))
+		buf = append(buf, x...)
 	}
-	sum := sha256.Sum256(buf.Bytes())
+	sum := sha256.Sum256(buf)
 
-	var b []byte
+	b := make([]byte, 0, int(sum[0]))
 	for i := 0; i < int(sum[0]); i++ {
 		x := sum[(i*7+1)%len(sum)] ^ sum[(i*5+3)%len(sum)]
-		c := strings.Repeat("abcdefghijklmnopqrstuvwxyz", 10)[x]
+		c := "abcdefghijklmnopqrstuvwxyz"[x%26]
 		b = append(b, c)
 	}
-	return string(b)
+	return *(*string)(unsafe.Pointer(&b))
 }
 
 func main() {
